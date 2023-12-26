c6789785cfd3eb3044eb6ef47b063cd98a35a73a
vladimir <v.chugunov@securitycode.ru>
2023-12-26 14:17:19 +0300

    add translation for Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,25 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLick, AppLinkTheme } from 'shared/ui/AppLink/AppLick';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLick theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                Главная
-            </AppLick>
-            <AppLick theme={AppLinkTheme.RED} to="/about">
-                О сайте
-            </AppLick>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLick theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                    {t('Главная')}
+                </AppLick>
+                <AppLick theme={AppLinkTheme.RED} to="/about">
+                    {t('О сайте')}
+                </AppLick>
+            </div>
         </div>
-    </div>
-);
+    );
+};
