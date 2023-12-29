78040fd9de9587f22df5aa86c9c410e730daf7cd
vladimir <v.chugunov@securitycode.ru>
2023-12-29 11:10:18 +0300

    add redux

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,18 @@
+// Импортируем конфиги из Redux Toolkit
+import { configureStore } from '@reduxjs/toolkit';
+import { StateSchema } from './StateSchema';
+
+// создаем функцию для переиспользования в сторибуке и тестов jest
+export function createReduxStore(initialState: StateSchema) {
+// Настраиваем конфиги redux Toolkit
+    return configureStore({
+        reducer: {},
+        devTools: __IS_DEV__, // отключаем devTools в продакшене
+        preloadedState: initialState,
+    });
+}
+
+// // Выведите типы RootState и AppDispatch из самого магазина.
+// export type RootState = ReturnType<typeof store.getState>
+// // Предполагаемый тип: {сообщения: PostsState, комментарии: CommentsState, пользователи: UsersState}
+// export type AppDispatch = typeof store.dispatch
