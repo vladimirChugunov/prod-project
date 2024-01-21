1dac3d1a54e91d08035466883b6506bc7a74adb1
vladimir <v.chugunov@securitycode.ru>
2024-01-21 14:43:48 +0300

    fix webpack storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,23 +10,24 @@ export default ({ config }: { config: webpack.Configuration }) => {
         html: '',
         src: path.resolve(__dirname, '../', '../', 'src'),
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('.tsx', '.ts', '.js');
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.tsx', '.ts', '.js');
     // проходимся по всем модулям из buildLoaders ищем в условии для svgLoader добавляем новое правило и исклбючаем svg
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: webpack.RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules = config!.module!.rules!.map((rule: webpack.RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
         // если svg нет то возвращаем просто rule
         return rule;
     });
-    config.module.rules.push({
+    config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module.rules.push(buildCssLoader(true));
-    config.plugins.push(new webpack.DefinePlugin({
+    config!.module!.rules.push(buildCssLoader(true));
+    config!.plugins!.push(new webpack.DefinePlugin({
         __IS_DEV__: JSON.stringify(true),
         __API__: JSON.stringify(''),
     }));
